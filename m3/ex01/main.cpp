0d6122b134bec4de96b68b0f8fd89ac3922d1fe2
Velovo <velovo.vanpraet@gmail.com>
2020-10-17 15:05:38 +0200

    fix main for ex01

--- m3/ex01/main.cpp
+++ m3/ex01/main.cpp
@@ -3,12 +3,32 @@
 
 int main(void)
 {
-	ScavTrap cl4 = ScavTrap("CLAP_TRAP");
-	FragTrap veronica = FragTrap("Veronica");
+	{
+		FragTrap cl4 = FragTrap("CLAP_TRAP");
+		FragTrap veronica = FragTrap("Veronica");
 
-	cl4.challengeNewcomer("Vault Hunter1");
-	cl4.challengeNewcomer("Moxxi");
-	cl4.challengeNewcomer("Roland");
-	cl4.challengeNewcomer("Vault Hunter2");
-	cl4.challengeNewcomer("Marcus");
+		cl4.meleeAttack("bandit");
+		veronica.rangedAttack("brutal bandit");
+		cl4.takeDamage(100);
+		//il lui reste 5 de vie
+		cl4.takeDamage(10);
+		cl4.beRepaired(100000);
+		cl4.takeDamage(1000);
+		cl4.beRepaired(10);
+		cl4.takeDamage(10);
+		veronica.vaulthunter_dot_exe("giant spider");
+		veronica.vaulthunter_dot_exe("BOOM BOOM");
+		veronica.vaulthunter_dot_exe("Scarlet");
+	}
+	std::cout << std::endl << std::endl << std::endl;
+	{
+		ScavTrap cl4 = ScavTrap("CLAP_TRAP");
+		ScavTrap veronica = ScavTrap("Veronica");
+
+		cl4.challengeNewcomer("Vault Hunter1");
+		cl4.challengeNewcomer("Moxxi");
+		cl4.challengeNewcomer("Roland");
+		veronica.challengeNewcomer("Vault Hunter2");
+		cl4.challengeNewcomer("Marcus");
+	}
 }
