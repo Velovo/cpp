b1a61129716946600b035b5d2bdf6ef6192c8120
Velovo <velovo.vanpraet@gmail.com>
2020-12-15 15:54:43 +0100

    main finished

--- m5/ex02/main.cpp
+++ m5/ex02/main.cpp
@@ -8,15 +8,57 @@ int main(void)
 {
 	try
 	{
-		Bureaucrat test = Bureaucrat("test", 135);
+		Bureaucrat test = Bureaucrat("test", 1);
 		ShrubberyCreationForm tree = ShrubberyCreationForm("maison");
+		PresidentialPardonForm pardon = PresidentialPardonForm("arthur");
+		RobotmyRequestForm perc = RobotmyRequestForm("mur");
 
 		test.signForm(tree);
+		test.signForm(pardon);
+		test.signForm(perc);
+
 		test.executeForm(tree);
+		test.executeForm(pardon);
+		test.executeForm(perc);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	
+	std::cout << std::endl << std::endl << std::endl;
+	try
+	{
+		Bureaucrat test1 = Bureaucrat("test1", 70);
+		Bureaucrat test2 = Bureaucrat("test2", 40);
+		RobotmyRequestForm perc = RobotmyRequestForm("maison");
+		test1.signForm(perc);
+		test2.executeForm(perc);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << std::endl << std::endl << std::endl;
+	try
+	{
+		Bureaucrat test = Bureaucrat("test", 70);
+		RobotmyRequestForm perc = RobotmyRequestForm("maison");
+		test.signForm(perc);
+		test.executeForm(perc);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	std::cout << std::endl << std::endl << std::endl;
+	try
+	{
+		Bureaucrat test = Bureaucrat("test", 5);
+		RobotmyRequestForm perc = RobotmyRequestForm("maison");
+		test.executeForm(perc);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
