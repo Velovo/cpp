28af4cd3fcc5d6f98a55f3c9078112e7fe368293
Velovo <velovo.vanpraet@gmail.com>
2021-01-14 13:54:03 +0100

    upgrade main

--- m7/ex00/main.cpp
+++ m7/ex00/main.cpp
@@ -1,19 +1,53 @@
 #include "whatever.hpp"
 #include <iostream>
 
+class Test1212
+{
+	private:
+	int _n;
+
+	public:
+	Test1212(int n) : _n(n) { };
+	bool operator==( Test1212 const & rhs ) { return (this->_n == rhs._n); }
+	bool operator!=( Test1212 const & rhs ) { return (this->_n != rhs._n); }
+	bool operator>( Test1212 const & rhs ) { return (this->_n > rhs._n); }
+	bool operator<( Test1212 const & rhs ) { return (this->_n < rhs._n); }
+	bool operator>=( Test1212 const & rhs ) { return (this->_n >= rhs._n); }
+	bool operator<=( Test1212 const & rhs ) { return (this->_n <= rhs._n); }
+	int getnbr() const { return _n; }
+};
+
+std::ostream &operator<<(std::ostream &os, const Test1212 &test1212)
+{
+	os << test1212.getnbr();
+	return os;
+}
+
 int main(void)
 {
-	int a = 2;
-	int b = 3;
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	{
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+	std::cout << std::endl << std::endl << std::endl;
+	{
+		Test1212 a(2);
+		Test1212 b(3);
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	}
+
 	return 0;
 }
--- m7/ex00/whatever.hpp
+++ m7/ex00/whatever.hpp
@@ -1,28 +1,30 @@
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
-#include <iostream>
+
+# include <iostream>
+
 template <class T>
 void swap(T &a, T &b)
 {
-    T c = a;
-    a = b;
-    b = c;
+	T c = a;
+	a = b;
+	b = c;
 }
 
 template <class T>
 T min(T a, T b)
 {
-    if (a < b)
-        return a;
-    return b;
+	if (a < b)
+		return a;
+	return b;
 }
 
 template <class T>
 T max(T a, T b)
 {
-    if (a > b)
-        return a;
-    return b;
+	if (a > b)
+		return a;
+	return b;
 }
 
 #endif
